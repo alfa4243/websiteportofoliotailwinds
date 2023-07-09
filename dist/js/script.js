0a0091b57f361f0807121eda7b32258b352c1383
alfa4243 <alfarijaalfarija@gmail.com>
2023-07-09 10:42:14 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,24 @@
+
+//navbar fixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+    };
+
+
+
+//hambuger
+const hamburger = document.querySelector('#hamburger');
+const navmenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navmenu.classList.toggle('hidden');
+
+});
